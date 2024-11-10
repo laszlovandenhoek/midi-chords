b98c93e819d8dcd5eba831f5c65813830acf2de5
László van den Hoek <laszlo.vandenhoek@gmail.com>
2024-11-10 12:08:30 +0100

    add an octave to the register

--- src/hooks/usePianoEvents.js
+++ src/hooks/usePianoEvents.js
@@ -2,11 +2,21 @@ import { useEffect, useRef } from 'react';
 import { MidiInputManager } from 'musicvis-lib';
 import { Note as TonalNote } from "tonal";
 
-const keyToPitch = {
+// "a" corresponds to A0, which is the lowest note on an 88-key piano.
+const keyToPitchNoShift = {
     'a': 21, 'w': 22, 's': 23, 'd': 24, 'r': 25, 'f': 26, 't': 27,
     'g': 28, 'h': 29, 'u': 30, 'j': 31, 'i': 32, 'k': 33, 'o': 34,
     'l': 35, ';': 36, '[': 37
 };
+
+//like keyToPitchNoShift but with shift key, shifted up by one octave.
+const keyToPitchWithShift = {
+    'A': 33, 'W': 34, 'S': 35, 'D': 36, 'R': 37, 'F': 38, 'T': 39,
+    'G': 40, 'H': 41, 'U': 42, 'J': 43, 'I': 44, 'K': 45, 'O': 46,
+    'L': 47, ':': 48, '{': 49
+};
+
+const keyToPitch = { ...keyToPitchNoShift, ...keyToPitchWithShift };
 
 // Create a singleton instance outside of the component
 let midiManagerInstance = null;
@@ -19,7 +29,7 @@ export function usePianoEvents(onNotePress, onNoteRelease) {
         // Keyboard handlers
         const handleKeyDown = (event) => {
             if (event.repeat) return;
-            const pitch = keyToPitch[event.key.toLowerCase()];
+            const pitch = keyToPitch[event.key];
             if (pitch) {
                 onNotePress({
                     pitch,
@@ -31,7 +41,7 @@ export function usePianoEvents(onNotePress, onNoteRelease) {
         };
 
         const handleKeyUp = (event) => {
-            const pitch = keyToPitch[event.key.toLowerCase()];
+            const pitch = keyToPitch[event.key];
             if (pitch) {
                 onNoteRelease({
                     pitch,
